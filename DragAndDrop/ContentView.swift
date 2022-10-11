622e85eff1728950dee1e8b4f609ab10bd6f6db0
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-10-11 14:27:39 -0400

    Initial Commit

---
+++ DragAndDrop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DragAndDrop
+//
+//  Created by Ryan Lintott on 2022-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DragAndDrop/DragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropApp.swift
+//  DragAndDrop
+//
+//  Created by Ryan Lintott on 2022-10-11.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
