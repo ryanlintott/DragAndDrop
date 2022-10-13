8bde03251c5bc962b751a48a3b9298c0160feafc
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-10-13 11:00:15 -0400

    Update ContentView lists to use separate arrays.

--- DragAndDrop/ContentView.swift
+++ DragAndDrop/ContentView.swift
@@ -8,46 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var birds = Bird.examples
-    
-    var birdList: some View {
-        List {
-            ForEach(birds) { bird in
-                Text(bird.name)
-                    .onDrag {
-                        bird.provider
-                    }
-            }
-            .onInsert(of: Bird.Wrapper.readableTypes) { index, itemProviders in
-                insertBird(position: index, itemProviders: itemProviders)
-            }
-            .onMove {
-                birds.move(fromOffsets: $0, toOffset: $1)
-            }
-            .onDelete {
-                birds.remove(atOffsets: $0)
-            }
-        }
-    }
+    @State private var birds1 = Bird.examples
+    @State private var birds2 = Bird.examples
     
     var body: some View {
         VStack {
-            birdList
+            List {
+                ForEach(birds1) { bird in
+                    Text(bird.name)
+                        .onDrag {
+                            bird.provider
+                        }
+                }
+                .onInsert(of: Bird.Wrapper.readableTypes) { index, itemProviders in
+                    itemProviders.reversed().loadItems(Bird.self) { bird, error in
+                        if let bird {
+                            birds1.insert(bird, at: index)
+                        }
+                    }
+                }
+                .onMove {
+                    birds1.move(fromOffsets: $0, toOffset: $1)
+                }
+                .onDelete {
+                    birds1.remove(atOffsets: $0)
+                }
+            }
             
-            birdList
+            List {
+                ForEach(birds2) { bird in
+                    Text(bird.name)
+                        .onDrag {
+                            bird.provider
+                        }
+                }
+                .onInsert(of: Bird.Wrapper.readableTypes) { index, itemProviders in
+                    itemProviders.reversed().loadItems(Bird.self) { bird, error in
+                        if let bird {
+                            birds2.insert(bird, at: index)
+                        }
+                    }
+                }
+                .onMove {
+                    birds2.move(fromOffsets: $0, toOffset: $1)
+                }
+                .onDelete {
+                    birds2.remove(atOffsets: $0)
+                }
+            }
             
             Text("Hand")
                 .onDrag { Bird(name: "HandBird").provider }
         }
         .padding()
-    }
-    
-    func insertBird(position: Int, itemProviders: [NSItemProvider]) {
-        itemProviders.reversed().loadItems(Bird.self) { bird, error in
-            if let bird {
-                birds.insert(bird, at: position)
-            }
-        }
     }
 }
 
